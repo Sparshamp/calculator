47660e6a0158f827d740ab284f66c06e62808112
Sparshamp <sparshamp7@gmail.com>
2025-10-17 08:52:16 +0530

    Added and Tested with negative values

--- tests/unit/test_calculator.py
+++ tests/unit/test_calculator.py
@@ -26,6 +26,16 @@ class TestBasicOperations:
         """Test subtracting positive numbers"""
         assert subtract(5, 3) == 2
         assert subtract(10, 4) == 6
+    
+    def test_add_negative_numbers(self):
+        """Test adding negative numbers"""
+        assert add(-1, -1) == -2
+        assert add(-5, 3) == -2
+
+    def test_add_negative_numbers(self):
+        """Test adding negative numbers"""
+        assert add(-1, -1) == -2
+        assert add(-5, 3) == -2
         
 class TestMultiplyDivide:
     """Test multiplication and division operations"""
